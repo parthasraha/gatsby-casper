8a81c7fd2b0442cc7aaac3adad198e40a16c6059
partha <parthasraha@gmail.com>
2022-09-06 21:33:20 +0530

    stable

--- sitebuild/blogsite/src/templates/index.tsx
+++ sitebuild/blogsite/src/templates/index.tsx
@@ -133,12 +133,12 @@ function IndexPage(props: IndexProps) {
 
 export const pageQuery = graphql`
   query blogPageQuery($skip: Int!, $limit: Int!) {
-    logo: file(relativePath: { eq: "img/ghost-logo.png" }) {
+    logo: file(relativePath: { eq: "ghost-logo.png" }) {
       childImageSharp {
         gatsbyImageData(layout: FIXED)
       }
     }
-    header: file(relativePath: { eq: "img/blog-cover.png" }) {
+    header: file(relativePath: { eq: "blog-cover.png" }) {
       childImageSharp {
         gatsbyImageData(width: 2000, quality: 100, layout: FIXED)
       }
